c0a5d324cb8dfe238b302b00fc48c41bbbf12789
Mickeyyok <boyman463347@gmail.com>
2025-10-30 01:42:59 +0700

    fix Kt Homepage

--- app/src/main/java/com/example/facecheckapp/HomepageActivity.kt
+++ app/src/main/java/com/example/facecheckapp/HomepageActivity.kt
@@ -1,11 +1,17 @@
 package com.example.facecheckapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-class HomePageActivity : AppCompatActivity() {
+class HomepageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homepage)
+
+
+        }
     }
-}
