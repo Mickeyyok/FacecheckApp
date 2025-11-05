d5fcd33b8f76d04b067cdb745ed21b12c0ab2480
Mickeyyok <boyman463347@gmail.com>
2025-11-06 02:34:24 +0700

    เพิ่มฟังชั่น add student

---
+++ app/src/main/java/com/example/facecheckapp/StudentData.kt
@@ -0,0 +1,8 @@
+package com.example.facecheckapp
+
+data class StudentData(
+    val first_name: String? = null,
+    val last_name: String? = null,
+    val id: String? = null,
+    val role: String? = null
+)
