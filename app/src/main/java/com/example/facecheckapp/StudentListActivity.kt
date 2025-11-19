32ead85646f88bf30dab197df3e1160d22adf357
Nonperchica <2310511101014@live4.utcc.ac.th>
2025-11-20 02:13:45 +0700

    เชื่อมหน้านักศึกษากับ รายวันกับรายเทอม + แก้ปุ่มปุ่มย้อนกลับ

--- app/src/main/java/com/example/facecheckapp/StudentListActivity.kt
+++ app/src/main/java/com/example/facecheckapp/StudentListActivity.kt
@@ -86,11 +86,15 @@ class StudentListActivity : AppCompatActivity() {
 
         // แท็บ Report (ยังไม่เปิดใช้งาน)
         tabReportDay.setOnClickListener {
-            Toast.makeText(this, "หน้านี้ยังไม่เปิดใช้งาน", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, RealTimeActivity::class.java)
+            intent.putExtra("classId", classId)
+            startActivity(intent)
         }
 
         tabReportTerm.setOnClickListener {
-            Toast.makeText(this, "หน้านี้ยังไม่เปิดใช้งาน", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, TermActivity::class.java)
+            intent.putExtra("classId", classId)
+            startActivity(intent)
         }
     }
 
